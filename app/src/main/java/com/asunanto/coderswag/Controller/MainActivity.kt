32a7f03e90f30713e33e1083cb70b6a815972011
wibow <wibowo.sutanto91@gmail.com>
2018-07-26 13:49:07 +1000

    added view holder

--- app/src/main/java/com/asunanto/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/asunanto/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.asunanto.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.asunanto.coderswag.Adapters.CategoryAdapter
 import com.asunanto.coderswag.Model.Category
 import com.asunanto.coderswag.R
 import com.asunanto.coderswag.Services.DataServices
@@ -10,15 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataServices.categories)
+        adapter = CategoryAdapter(this,DataServices.categories)
 
         categoryListView.adapter = adapter
     }
